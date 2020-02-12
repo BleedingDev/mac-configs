fd6c9ac1a4f8d194a5ed7637c33782dab33ac2c9
Petr Glaser <petr.glaser@Petrs-MacBook-Pro-2.local>
2020-02-12 15:51:47 +0100

    Initial commit

---
+++ fish/config.fish
@@ -0,0 +1,7 @@
+alias gmm='git checkout master; git pull; git checkout -; git merge origin/master'
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/usr/local/opt/nvm/nvm.sh" ] && . "/usr/local/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/usr/local/opt/nvm/etc/bash_completion" ] && . "/usr/local/opt/nvm/etc/bash_completion"  # This loads nvm bash_completion
+
+source ~/.iterm2_shell_integration.(basename $SHELL)
---
+++ fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/Users/petrglaser/.local/share/omf/themes/batman/fish_prompt.fish
